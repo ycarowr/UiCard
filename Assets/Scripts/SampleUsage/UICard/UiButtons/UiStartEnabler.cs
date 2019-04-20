bb551673da40e3f8ca46e2170d6aff0d62ffe208
ycarowr <ywrycaro@gmail.com>
2019-04-20 20:04:59 +0300

    Add enabled for configs

---
+++ Assets/Scripts/SampleUsage/UICard/UiButtons/UiStartEnabler.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Tools.UI.Card
+{
+    public class UiStartEnabler : MonoBehaviour
+    {
+        public bool IsActive;
+        void Start()
+        {
+            gameObject.SetActive(IsActive);
+        }
+    }
+}
